cadb01e7caebccce8c94695f7106d2c406e0a1ff
Hassan Shaikh <shaikh.hassan01@outlook.com>
2022-05-07 16:12:30 -0400

    added the CRUD methods

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { param } = require("express/lib/request");
 const res = require("express/lib/response");
 const mysql = require("mysql2");
+const inputCheck = require("./utils/inputCheck");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -20,19 +21,6 @@ const db = mysql.createConnection(
   },
   console.log("Connected to the election database.")
 );
-
-// // //Create a candidate
-// // const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-// //               VALUES (?,?,?,?)`;
-// // const params = [1, "Ronald", "Firbank", 1];
-
-// // db.query(sql, params, (err, result) => {
-// //   if (err) {
-// //     console.log(err);
-// //   } else {
-// //     console.log(result);
-// //   }
-// // });
 
 // Get all candidates
 app.get("/api/candidates", (req, res) => {
@@ -89,12 +77,40 @@ app.delete("/api/candidate/:id", (req, res) => {
   });
 });
 
-//Default response for any other request (Not Found)
+// Create a candidate
+app.post("/api/candidate", ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    "first_name",
+    "last_name",
+    "industry_connected"
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
+    });
+  });
+});
+
+// Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
 
-//start the express.js server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
